4250621c0243732995d834068ba1c85b9bb2d48f
vinesh-git <chawanvinesh2001@gmail.com>
2025-09-25 00:06:54 +0530

    Added global styles

---
+++ assets/styles/globalStyles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    backgroundWhite: {
+        backgroundColor: '#ffffff'
+    },
+    flex:{
+        flex:1
+    }
+})
+export default style;
--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,10 +1,10 @@
 import { View,Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import style from "../../assets/styles/globalStyles";
 
 const Home = () => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={[style.flex,style.backgroundWhite]}>
             <View>
                 <Text>Home Screen</Text>
             </View>
